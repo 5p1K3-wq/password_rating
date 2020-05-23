422476b3f57a8a272bbdeb4cf9b62c2476a656d3
spike <kalinin_ks@outlook.com>
2020-05-23 11:11:49 +0300

    Fix

--- main.py
+++ main.py
@@ -26,26 +26,29 @@ def doesnt_consist_of_symbols(password):
 
 
 def is_very_long(password):
-    if len(password) < 12:
-        return False
-    return True
+    return not len(password) < 12
 
 
-def on_change_password(edit, new_edit_text):
+def on_change_password(edit, new_edit_text, functions_password_verification=[]):
     score = 0
-
-    for checking_password in PASSWORD_VERIFICATION:
+    for checking_password in functions_password_verification:
         if checking_password(new_edit_text):
             score += 2
     fill.set_text('Рейтинг пароля: %s' % score)
 
 
+def on_exit_clicked(button):
+    raise urwid.ExitMainLoop()
+
+
 if __name__ == "__main__":
-    PASSWORD_VERIFICATION = [has_digit, is_very_long, has_letters, has_upper_letters, has_lower_letters, has_symbols,
+    password_verification = [has_digit, is_very_long, has_letters, has_upper_letters, has_lower_letters, has_symbols,
                              doesnt_consist_of_symbols]
     text = urwid.Edit(u'Введите пароль: ', mask='*')
     fill = urwid.Text('')
-    menu = urwid.Pile([text, fill])
+    button_exit = urwid.Button(u'Exit')
+    menu = urwid.Pile([text, fill, button_exit])
     menu = urwid.Filler(menu, valign='top')
-    urwid.connect_signal(text, 'change', on_change_password)
+    urwid.connect_signal(text, "change", on_change_password, password_verification)
+    urwid.connect_signal(button_exit, 'click', on_exit_clicked)
     urwid.MainLoop(menu).run()
